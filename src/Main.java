e4118d0b134733ceeb096245d1ed7776d8df8bba
cjwcommuny <cjwcommuny@outlook.com>
2018-10-24 11:40:38 +0800

    add frame (not completed)

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
+import java.util.*;
+import java.io.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        InputStream inputStream = System.in;
+        PrintStream outpuStream = System.out;
+        Scanner s = new Scanner(inputStream);
+        while (true) {
+            System.out.print(InteractiveInterface.promptStr); //TODO: 重定向至其他流
+            String instruction = s.nextLine();
+            if (instruction.equals(InteractiveInterface.exitCommand)) { //TODO
+                //TODO: add prompt when leave
+                break;
+            }
+            outpuStream.println(instruction);
+        }
     }
 }
